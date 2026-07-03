0a68163cf6d68329489f4be3265e28c82e26c1c1
Marat Mestnikov <ma.mestnikov@gmail.com>
2026-07-03 13:45:24 +0000

    config quality of life (lsp)

--- viki/config.py
+++ viki/config.py
@@ -8,9 +8,48 @@ Loaded from data/user_configuration.json.
 import json
 import os
 import shutil
+from typing import Any
 
 DEFAULT_CONFIG_PATH = "data/default_configuration.json"
 USER_CONFIG_PATH = "data/user_configuration.json"
+
+# Duck variables which LSP can catch and use
+INTRINSICS_FILENAME: str
+EXTRINSICS_FILENAME: str
+DEFAULT_FPS: int
+DEFAULT_COLOR_WIDTH: int
+DEFAULT_COLOR_HEIGHT: int
+DEFAULT_DEPTH_MODE: str
+JPEG_QUALITY: int
+STREAM_IDLE_SLEEP: float
+PLACEHOLDER_SIZE: list[int]
+DEFAULT_SYNCHRONIZED_IMAGES_ONLY: bool
+FRAME_BUFFER_SIZE: int
+RECORD_DEPTH: bool
+DEPTH_PROJECTION_DEBUG: bool
+SKELETON_DEPTH_SAMP_RADIUS: int
+SKELETON_DEPTH_BASE_DIR: str
+SKELETON_ENABLE_DEPTH_VALIDATION: bool
+SKELETON_DEPTH_SUBTRACT_THRESHOLD: float
+HAND_TO_DETECT: str
+CALIB_MODE: str
+CALIB_BOARD_TYPE: str
+BONE_LENGTHS: dict[str, Any]
+BONE_TOLERANCE: float
+CALIB_CHESS_BOARD_SIZE: list[int]
+CALIB_CHESS_SQUARE_SIZE: float
+CALIB_ARUCO_BOARD_SIZE: list[int]
+CALIB_ARUCO_SQUARE_SIZE: float
+CALIB_ARUCO_MARKER_SIZE: float
+CALIB_ARUCO_DICT: int
+RECORDING_DURATION: int
+RECORDING_FPS: int
+REALSENSE_RESOLUTIONS: list[str]
+REALSENSE_FPS: list[int]
+KINECT_RESOLUTIONS: list[str]
+KINECT_FPS: list[int]
+KINECT_DEPTH_MODES: list[str]
+KINECT_DEPTH_MODE_MAX_FPS: dict[str, int]
 
 
 def _load_config():
